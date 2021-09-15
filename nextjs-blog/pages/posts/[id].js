9ea5ebaaf0bb02730a2e401f7eca6d839f2bf981
Gregory McNally <89556692+gmcnally78@users.noreply.github.com>
2021-09-15 03:46:51 +0000

    Dynamic Routes Polishing the Post Page (page 7)

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,15 +1,45 @@
+// Add this import at the top of the file
+import utilStyles from '../../styles/utils.module.css'
+// Add this import
+import Date from '../../components/date'
+// Add this import
+import Head from 'next/head'
+
 import Layout from '../../components/layout'
 
-import { getAllPostIds } from '../../lib/posts'
+import { getAllPostIds, getPostData } from '../../lib/posts'
 
-export async function getStaticPaths() {
-  const paths = getAllPostIds()
+export async function getStaticProps({ params }) {
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id)
+  // ...
   return {
-    paths,
-    fallback: false
+    props: {
+      postData
+    }
   }
 }
 
-export default function Post() {
-  return <Layout>...</Layout>
+export default function Post({ postData }) {
+  return (
+    <Layout>
+     <Head>
+        <title>{postData.title}</title>
+      </Head>
+       <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+       <Date dateString={postData.date} />
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  )
 }
