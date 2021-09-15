80f946d26491cd4cb4fa4d103ff9b080a6c98a8f
Gregory McNally <89556692+gmcnally78@users.noreply.github.com>
2021-09-15 01:46:18 +0000

    Started over on Dynamics Routes Page 4

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,12 +1,6 @@
 import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
 
-
-export async function getStaticProps({ params }) {
-  // Add the "await" keyword like this:
-  const postData = await getPostData(params.id)
-  // ...
-}
+import { getAllPostIds } from '../../lib/posts'
 
 export async function getStaticPaths() {
   const paths = getAllPostIds()
@@ -15,16 +9,7 @@ export async function getStaticPaths() {
     fallback: false
   }
 }
-export default function Post({ postData }) {
-  return (
-    <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-    </Layout>
-  )
+
+export default function Post() {
+  return <Layout>...</Layout>
 }
